0108dbd490c7701cd9a83bef1521e1295c03d926
ashishkumardubey123 <ashish9039062705@gmail.com>
2024-09-22 02:36:32 +0530

    creteated a rename funcnality

--- first_project/index.js
+++ first_project/index.js
@@ -1,67 +1,78 @@
-const express  = require("express");
+const express = require("express");
 const app = express();
-const path= require('path')
-const fs = require('fs');
-
-
-
+const path = require("path");
+const fs = require("fs");
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
- app.use(express.static(path.join(__dirname, 'public')))
- app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
 
-
- app.get('/',function(req, res){
-  fs.readdir(`./files`, function(err,  files){
-    res.render('index', {files:files} )
-  } )
-})
-
-app.get('/files/:filename',function(req, res){
-     
- fs.readFile(`./files/${req.params.filename}`, 'utf8', function(err, data){
-    if(err) res.send(err)
-      
-    else {
-
-    res.render('show', {filename:req.params.filename, filedata:data } )
-    }
-  } )
-
-  } )
-
-
-app.post('/create', function(req, res) {
-    
-    const { title, details } = req.body;
- fs.writeFile(`./files/${title.split(' ').join('')}.txt`,details, function(err){
-    
-    res.redirect('/' ); 
- } )
-
+app.get("/", function (req, res) {
+  fs.readdir(`./files`, function (err, files) {
+    res.render("index", { files: files });
+  });
 });
-app.post ('/delete', function(req, res) {
-  const filename = req.body.filename;
-   fs.unlink(`./files/${filename}`,(err)=>{
-    if(err) res.send(err.message)
-      res.redirect('/' ); 
-   } )
 
+app.get("/files/:filename", function (req, res) {
+  fs.readFile(`./files/${req.params.filename}`, "utf8", function (err, data) {
+    if (err) res.send(err);
+    else {
+      res.render("show", { filename: req.params.filename, filedata: data });
+    }
+  });
+});
 
-} ) 
-app.post ('/delete', function(req, res) {
-  const filename = req.body.filename;
-   fs.unlink(`./files/${filename}`,(err)=>{
-    if(err) res.send(err.message)
-      res.redirect('/' ); 
-   } )
+app.post("/create", function (req, res) {
+  const { title, details } = req.body;
+  fs.writeFile(
+    `./files/${title.split(" ").join("")}.txt`,
+    details,
+    function (err) {
+      res.redirect("/");
+    }
+  );
+});
 
+app.get("/edit/:filename", function (req, res) {
+ 
+  res.render("edit", {filename: req.params.filename} )
+  
+});
+ 
+app.post("/edit", function (req, res) {
+ 
 
-} )
+fs.rename(`./files/${req.body.previous}` , `./files/${req.body.new}.txt`, function(err){
+       
+  console.log(req.body)
+  
+    res.redirect("/");
 
-
-app.listen(3000, function(err){
-    if(err) console.log(err.message)
-    else console.log('server is running on port 3000')
 })
+  
+
+
+  
+});
+
+
+
+app.post("/delete", function (req, res) {
+  const filename = req.body.filename;
+  fs.unlink(`./files/${filename}`, (err) => {
+    if (err) res.send(err.message);
+    res.redirect("/");
+  });
+});
+
+
+
+ 
+
+
+
+app.listen(3000, function (err) {
+  if (err) console.log(err.message);
+  else console.log("server is running on port 3000");
+});
